925babd535ece5b2b6288a2ffebc6be4575a0fbd
Tucker Mullen <mullentucker@gmail.com>
2016-03-08 17:59:23 -0800

    Wiki CRUD complete

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,55 @@
+class WikisController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
+  
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "Error saving Wiki. Try again."
+      render :new
+    end
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+      flash[:notice] = "Wiki was updated."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "Error saving the Wiki. Try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Error deleting Wiki. Try again."
+      render :show
+    end
+  end
+end
