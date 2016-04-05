82f0630681ce990e74200d953d23cb02afb26d14
Tucker Mullen <mullentucker@gmail.com>
2016-04-05 10:32:43 -0700

    Error within charges partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
     @user = User.find(params[:id])
-    @wiki = Wiki.new
+    @wiki = Wiki.find(params[:id])
     @wikis = @user.wikis
   end
 
   def downgrade
-    current_user.update_attributes!(role: 'standard')
-
-    if current_user.role == 'standard'
+    if current_user.role == 'premium'
+      current_user.update_attributes!(role: 'standard')
       flash[:notice] = "Membership Downgraded!"
+      redirect_to user_path(current_user)
     else
       flash[:error] = "There was an error downgrading your membership. Please try again."
+      redirect_to user_path(current_user)
     end
   end
 end
