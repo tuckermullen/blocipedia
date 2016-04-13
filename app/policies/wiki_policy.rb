2727b83e695526c9e62e6e7981ee246cdcaf9bf9
Tucker Mullen <mullentucker@gmail.com>
2016-04-13 11:22:21 -0700

    In progress, working on wiki scope 4/13

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -50,7 +50,7 @@ class Scope
      elsif user.role == 'premium'
        all_wikis = scope.all
        all_wikis.each do |wiki|
-         if wiki.private? == false || wiki.owner == user || wiki.collaborators.include?(user)
+         if wiki.private? == false || wiki.user == user || wiki.collaborators.include?(user)
            wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
          end
        end
